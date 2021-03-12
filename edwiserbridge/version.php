bb5eab580b673a866b862f572aa02a0c85f6f5f0
sudam.chakor <sudam.chakor@wisdmlabs.com>
2021-03-12 12:13:07 +0530

    Changed the plugin version.

--- edwiserbridge/version.php
+++ edwiserbridge/version.php
@@ -21,7 +21,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021012201;
+$plugin->version   = 2021031200;
 $plugin->component = 'local_edwiserbridge';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.0.5';
+$plugin->release   = '2.0.6';
