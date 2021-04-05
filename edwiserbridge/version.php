38968cbd74285ddd3bd34f15fa21dabbe3e0ba7a
Kunal Kamble <krunal.kamble@wisdmlabs.com>
2021-04-05 18:41:57 +0530

    refs #68772: Made Moodle plugin GDPR compliant.

--- edwiserbridge/version.php
+++ edwiserbridge/version.php
@@ -21,7 +21,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021031200;
+$plugin->version   = 2021031500;
 $plugin->component = 'local_edwiserbridge';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '2.0.6';
