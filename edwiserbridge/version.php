0a80f315094d003e4b35432eb95e214a5b6a3e5c
ishwar.singh <ishwar.singh.solanki@wisdmlabs.com>
2022-12-28 03:32:54 +0530

    updated version number for final release 2.2.6

--- edwiserbridge/version.php
+++ edwiserbridge/version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2022122816;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = '2.2.3';
+$plugin->release   = '2.2.6';
 $plugin->requires  = '2016052318'; // Requires this Moodle version (Moodle V3.1.0).
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'local_edwiserbridge'; // Full name of the plugin (used for diagnostics).
